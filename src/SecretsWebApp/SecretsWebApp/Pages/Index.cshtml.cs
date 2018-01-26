372152264476e1b16fa45a95a189ccdb7504bf3a
James Hollyhomes <Jamie.Hollyhomes@hotmail.co.uk>
2018-01-26 13:21:56 +0000

    created initial test project

---
+++ src/SecretsWebApp/SecretsWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace SecretsWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string SecretValue { get; set; }
+
+        public IConfiguration Configuration { get; }
+
+        public void OnGet()
+        {
+            SecretValue = Configuration["MySecret"];
+        }
+    }
+}
---
+++ src/SecretsWebApp/SecretsWebApp/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SecretsWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
